75ddda7a2bf6145f5ba340dee3466ba465c7a814
Shliachkov Mikhail <Mikhail.Shliachkov@t-systems.ru>
2016-08-07 23:04:43 +0300

    refactoring

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 define('ROOT', dirname(__DIR__) . '/php/');
 
-$account_name = 'Исполнитель 115';
-$acc_balance = "0$";
+require_once 'php/model/user/user.php';
+
+$user = get_user($_GET['login'], $_GET['pwd']);
+
+$account_name = $user['name'];
+$acc_balance = $user['balance'];
 $sys_balance = "100$";
-require_once ROOT . 'view/template_view.php';
+require_once dirname(__DIR__) . '/vk-task/php/view/template_view.php';
--- php/model/user/user.php
+++ php/model/user/user.php
@@ -1,11 +1,36 @@
 <?php
 
+require_once dirname(__DIR__) . '/db.php';
+
 define('USER_TYPE_WORKER', 1);
 define('USER_TYPE_EMPLOYER', 2);
 
-define('PWD_HASH_OPTS', ['cost' => 12]);
+define('PWD_HASH_COST', 12);
 
 function login($user_login, $user_pwd_hash)
 {
 
+}
+
+function get_user($login, $pwd)
+{
+    $q = "SELECT id, login, username, password_hash, account_type, balance FROM users WHERE login=?";
+
+    $res = query(USERS_DB, $q, 's', $login);
+    if ($res) {
+        if (password_verify($pwd, $res[3])) {
+            return convert_db_row_to_user($res);
+        }
+    }
+
+    return false;
+}
+
+function convert_db_row_to_user($res)
+{
+    return ['id' => $res[0],
+        'name' => $res[2],
+        'type' => $res[4],
+        'balance' => $res[5]
+    ];
 }
